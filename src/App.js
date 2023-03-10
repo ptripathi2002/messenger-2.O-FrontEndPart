1517b3547c713ba68de87de5204ec70aaa9da111
Prashant Tripathi <itsmeprashant02@gmail.com>
2023-03-10 23:26:02 +0530

    first-commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Login from "./components/Login";
+import Messenger from "./components/Messenger";
+import Register from "./components/Register";
+import ProtectRoute from "./components/ProtectRoute";
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+    <Routes>
+      <Route path="/messenger/login" element={<Login />} />
+      <Route path="/messenger/register" element={<Register />} /> 
+      <Route path="/" element={ <ProtectRoute> <Messenger /> </ProtectRoute> } />
+    </Routes>
+  </BrowserRouter>,
+
+    </div>
+  );
+}
+export default App;
